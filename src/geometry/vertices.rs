244468b5b114242b2aca55ea03a8a91b6b00993d
sam-pearmain <sam.pearmain@kcl.ac.uk>
2025-02-25 12:53:12 +0000

    removed write order, everything just ijk now

--- src/geometry/vertices.rs
+++ src/geometry/vertices.rs
@@ -1,20 +1,15 @@
 #![allow(dead_code)]
 
 use std::marker::PhantomData;
+
 use super::points::{Dimensioned, Point, Dimensions, Point2D, Point3D};
 use super::vertex::Vertex;
-
-pub enum WriteOrder {
-    IJK, // write row, then column, then layer
-    JIK, // write column, then row, then layer
-}
 
 /// should be able to store both 2d and 3d sets of vertices and 
 /// have common functoinality to interpret the vertices
 pub struct VertexCollection<P: Point> {
     vertices: Vec<Vertex<P>>,
     dimensions: Dimensions,
-    write_order: WriteOrder,
 }
 
 impl<P: Point> Dimensioned for VertexCollection<P> {
@@ -44,25 +39,20 @@ impl<P: Point> VertexCollection<P> {
 }
 
 pub struct VertexCollectionBuilder<P: Point> {
-    dimensions: Dimensions,
-    write_order: Option<WriteOrder>,
+    dimensions: Option<Dimensions>,
     _phantom: PhantomData<P>,
 }
 
 impl<P: Point> VertexCollectionBuilder<P> {
-    pub fn set_write_order(mut self, write_order: WriteOrder) -> Self {
-        self.write_order = Some(write_order);
-        self
-    }
-
     pub fn build(self) -> Result<VertexCollection<P>, &'static str> {
-        if self.write_order.is_none() {
-            return Err("write order not set, cannot build")
-        }
+        let dimensions = match self.dimensions {
+            Some(d) => d,
+            None => return Err("dimensions must be set before attempting to build a vertex collection"),
+        };
+        
         Ok(VertexCollection {
             vertices: Vec::new(),
-            dimensions: self.dimensions,
-            write_order: self.write_order.unwrap(),
+            dimensions
         })
     }
 }
@@ -70,18 +60,16 @@ impl<P: Point> VertexCollectionBuilder<P> {
 impl VertexCollectionBuilder<Point2D> {
     pub fn new_2d(nx: usize, ny: usize) -> Self {
         VertexCollectionBuilder {
-            dimensions: Dimensions::Two { nx, ny },
-            write_order: None,
+            dimensions: Some(Dimensions::Two { nx, ny }),
             _phantom: PhantomData,
         }
     }
 }
 
 impl VertexCollectionBuilder<Point3D> {
-    pub fn new_3d_f64(nx: usize, ny: usize, nz: usize) -> Self {
+    pub fn new_3d(nx: usize, ny: usize, nz: usize) -> Self {
         VertexCollectionBuilder {
-            dimensions: Dimensions::Three { nx, ny, nz },
-            write_order: None, 
+            dimensions: Some(Dimensions::Three { nx, ny, nz }),
             _phantom: PhantomData,
         }
     }
@@ -93,37 +81,8 @@ mod tests {
 
     #[test]
     fn test_vertex_collection_builder() {
-        let collection_2d = VertexCollectionBuilder::new_2d(9, 9)
-            .set_write_order(WriteOrder::IJK)
+        let collection = VertexCollectionBuilder::new_2d(10, 10)
             .build();
-        assert!(collection_2d.is_ok());
-    }
-
-    #[test]
-    fn test_2d_builder_success() {
-        let vc = VertexCollectionBuilder::new_2d(10, 20)
-            .set_write_order(WriteOrder::IJK)
-            .build();
-        assert!(vc.is_ok(), "2D builder should build successfully");
-    }
-
-    #[test]
-    fn test_builder_missing_write_order() {
-        let vc = VertexCollectionBuilder::new_2d(10, 20)
-            .build();
-        assert!(vc.is_err(), "Builder with no write order should error");
-    }
-
-    #[test]
-    fn test_add_vertex_to_collection() {
-        let mut collection = VertexCollectionBuilder::new_2d(10, 20)
-            .set_write_order(WriteOrder::IJK)
-            .build()
-            .expect("Builder should succeed");
-
-        let vertex = Vertex::new_2d(1, 10.0, 10.0);
-        collection.add_vertex(vertex);
-
-        assert_eq!(collection.vertices.len(), 1);
+        assert!(collection.is_ok());
     }
 }
--- src/mesh/block.rs
+++ src/mesh/block.rs
@@ -2,20 +2,39 @@
 
 use std::marker::PhantomData;
 use crate::geometry::prelude::*;
-use crate::mesh::nodes::Node;
+use crate::mesh::nodes::{Node, NodeCollection};
 
 pub enum BoundaryType {
-    Vertical, 
-    Horizontal, 
-
+    North,  // 2D
+    South,  // 2D
+    East,   // 2D
+    West,   // 2D
+    Top,    // 3D
+    Bottom, // 3D
 }
 
 pub struct Block<'a, P: Point> {
     pub id: usize, 
-    pub vertex_collection: VertexCollection<P>,
-    pub nodes: Vec<Node<'a, P>>, // should probably be a node collection
+    pub vertices: VertexCollection<P>,
+    pub nodes: NodeCollection<'a, P>, 
 }
 
-pub struct BlockBuilder<P: Point> {
+pub struct BlockBuilder<'a, P: Point> {
+    id: usize, 
+    dimensions: Option<Dimensions>,
+    vertex_collection_builder: Option<VertexCollectionBuilder<P>>,
+    node_collection_builder: Option<NodeCollection<'a, P>>,
     _phantom: PhantomData<P>,
+}
+
+impl<'a, P: Point> BlockBuilder<'a, P> {
+    fn new(id: usize) -> Self {
+        BlockBuilder {
+            id, 
+            dimensions: None, 
+            vertex_collection_builder: None,
+            node_collection_builder: None, 
+            _phantom: PhantomData,
+        }
+    }
 }
